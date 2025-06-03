5498436d450b1a5a3a720b178001f016b97cb498
sgajans <bilgi@sgajansmuhendislik.com>
2025-06-03 05:27:36 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SGReklamApp from './SGReklamApp.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SGReklamApp />
+  </React.StrictMode>,
+)
